b6ab8d1d4ce2263df81f778d8741ddf16fd9023d
sakib <mohammadsakib7679@gmail.com>
2024-07-13 11:30:14 +0600

    use try catch for error handle

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
+import { StatusCodes } from "http-status-codes";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Admin created successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: error?.name || "Something went wrong, Admin not created!",
+      error: error,
+    });
+  }
 };
 
 export const userController = {
