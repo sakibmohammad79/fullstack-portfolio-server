cf5556e7eb3fa2aa0559aaf1209e91dfafb2107c
sakib <mohammadsakib7679@gmail.com>
2024-07-28 12:17:23 +0600

    update blog

--- src/app/modules/Blog/blog.validation.ts
+++ src/app/modules/Blog/blog.validation.ts
@@ -3,8 +3,10 @@ import { z } from "zod";
 const createBlogValidationSchema = z.object({
   body: z.object({
     id: z.number().int().optional(),
+    name: z.string(),
     title: z.string(),
     content: z.string(),
+    image: z.string(),
     published: z.boolean().default(false),
     userId: z.string(),
     createdAt: z.date().optional(),
@@ -14,8 +16,10 @@ const createBlogValidationSchema = z.object({
 const updateBlogValidationSchema = z.object({
   body: z.object({
     id: z.number().int().optional(),
+    name: z.string().optional(),
     title: z.string().optional(),
     content: z.string().optional(),
+    image: z.string().optional(),
     published: z.boolean().default(false).optional(),
     userId: z.string().optional(),
     createdAt: z.date().optional(),
--- src/config/index.ts
+++ src/config/index.ts
@@ -13,3 +13,6 @@ export default {
     refresh_token_expires_in: process.env.REFRESH_TOKEN_EXP,
   },
 };
+
+//"postgresql://postgres:718278@localhost:5432/my_portfolio?schema=public"
+//"postgresql://postgres.zqvsviriupvsopyewucw:sa79kib..sa79kib..79@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
